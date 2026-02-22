60276a477e6e8fbed01f4ee78a12260d60763238
Gedjur <gedsvk@gmail.com>
2026-02-22 15:16:56 +0100

    Solve exercise 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = "";
+    for (let index = input.length-1; index >= 0; index--) {   
+           output += input[index];
+    }
+    return output;
 };
 
 // Do not edit below this line
